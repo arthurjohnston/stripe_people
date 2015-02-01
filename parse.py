09e25dc056e18298e2d24e623fdf9ddad2e404bb
Arthur <arthur.j.johnston@gmail.com>
2015-01-31 22:04:33 -0500

    cleaned up names

--- parse.py
+++ parse.py
@@ -4,13 +4,14 @@ import re
 
 stop = stopwords.words('english')
 soup=bs(open("people.html"))
-people=soup.findAll('span',{"class":"inner"})
-rawtext=rawtext=''.join([p.text.replace('\n','').replace(',','').lower() for p in people])
-tokens=re.split('\W+',rawtext)
+raw_bios=soup.findAll('span',{"class":"inner"})
+bios=[b.text.replace('\n','').replace(',','').lower() for b in raw_bios]
+text=''.join(bios)
+tokens=re.split('\W+',text)
 wordCount=dict()
 for t in (t for t in tokens if t not in stop):
 	if t not in wordCount:
 		wordCount[t]=1
 	else:
 		wordCount[t]+=1
-sorted(wordCount.items(),key=lambda x:x[1])
+sorted(wordCount.items(),key=lambda x:x[1],reverse=True)
